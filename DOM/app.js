59cf5998cc950206ee79069015d971057891e174
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-10 19:23:00 +0530

    Dom Selectors

--- DOM/app.js
+++ DOM/app.js
@@ -2,8 +2,28 @@
 // console.log(document.querySelector('#task-title'));
 // console.log(document.querySelector('.card-title'));
 // console.log(document.querySelector('h5'));
-
 // document.querySelector('li').style.color='red';
 // document.querySelector('li:last-child').style.color='red';
 
-//
+//document.getElementsByClassName
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[4].style.color = 'red';
+
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
+
+//query SSelectorsAll
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item,index){
+    //console.log(item.className);
+    item.textContent = `${index}: Hello`;
+});
+
+console.log(items);
+
