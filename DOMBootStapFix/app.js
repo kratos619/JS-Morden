94f395be76367a8754659ad7b0746e0dc2d29b75
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-10 23:46:12 +0530

    BootStrap Row Col grid fix

--- DOMBootStapFix/app.js
+++ DOMBootStapFix/app.js
@@ -1,7 +1,8 @@
-let row ;
+// create element
+let row;
 let col;
-const parentElementRow = document.querySelector('div.row');
-const chileElement = document.querySelector('div.col-md-6');
+const parentElementRow = document.querySelector("div.row");
+const chileElement = document.querySelector("div.col-md-6");
 
 //geting parent element
 console.log(parentElementRow);
@@ -15,13 +16,27 @@ row = parentElementRow.children;
 //    row =  document.createElement('div');
 // console.log(row);
 // }
-
+var first_row = parentElementRow.parentElement.lastElementChild;
+console.log(first_row);
+var new_row = parentElementRow.parentElement.lastElementChild;
 numberofchildElementofrow = parentElementRow.childElementCount;
 console.log(numberofchildElementofrow);
-if(numberofchildElementofrow >= 2){
-    row = parentElementRow.parentElement;
-    console.log(row);
-    row =  document.createElement('div');
-    console.log('div Created');
-    console.log(row);
+if (numberofchildElementofrow > 2) {
+  var col2;
+  row = document.createElement("div");
+  row.className = "row";
+  col = document.createElement("div");
+  col.className = "col-md-6";
+  col2 = document.createElement("div");
+  col2.className = "col-md-6";
+  console.log(col2);
+  row.appendChild(col);
+  row.appendChild(col2);
+  console.log(row);
+  col.appendChild(document.createTextNode("hi"));
+  col2.appendChild(document.createTextNode("hellow"));
+  document.querySelector("div.container").appendChild(row);
+  console.log(row);
+
+  //console.log(col);
 }
