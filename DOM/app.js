48ae599867bb18bf4ffc78463a22383a565bb419
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-10 18:48:03 +0530

    Query Selectors
    Note query selectors are good but the can affect onlyy one item even using class

---
+++ DOM/app.js
@@ -0,0 +1,9 @@
+//query SSelectors
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color='red';
+// document.querySelector('li:last-child').style.color='red';
+
+//
