4c34d6763c2e7260a7d16d459f37c99162929641
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-12 22:46:41 +0530

    LocalStorage And Session

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,21 @@
-// Local Storage And Session start
+//Local Storage And Session start
+// localStorage.setItem("name", "jhon");
+
+// set  Session storage Item
+//sessionStorage.setItem("name", "jhon");
+
+// remove from storage
+// localStorage.removeItem("name");
+// sessionStorage.removeItem("name");
+
+// get data from storage
+// const name = localStorage.getItem("name");
+// console.log(name);
+// // clear localstorage
+// localStorage.clear(name);
+
+document.getSelection("from").addEventListener("submit", function(e) {
+  const task = document.getElementById("task").value;
+  console.log(task);
+  e.preventDefault();
+});
