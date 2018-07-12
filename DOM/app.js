945324e677a9cfac3fc542f199e14c7c5106250c
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-12 22:21:51 +0530

    Event Delegation and Event Bubbling Super Imp

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,31 @@
-//Event Bubbling
+// //Event Bubbling
+
+// document.querySelector(".card-title").addEventListener("click", function() {
+//   console.log("card-title");
+// });
+
+// document.querySelector(".card-content").addEventListener("click", function() {
+//   console.log("card-content");
+// });
+
+// document.querySelector(".card").addEventListener("click", function() {
+//   console.log("card");
+// });
+
+// document.querySelector(".col").addEventListener("click", function() {
+//   console.log("col");
+// });
+
+//Event Delegation
+
+document.body.addEventListener("click", deleteItem);
+
+function deleteItem(e) {
+  // if (e.target.parentElement.className == "delete-item secondary-content") {
+  //   console.log("delete Item");
+  // }
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    console.log("delete Item");
+    e.target.parentElement.parentElement.remove();
+  }
+}
