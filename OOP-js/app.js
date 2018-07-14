27c5afef06acd74c79c6f8a58796b9475de355ba
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-14 21:45:34 +0530

    Inheritance

--- OOP-js/app.js
+++ OOP-js/app.js
@@ -1,47 +1,35 @@
-// Object.Proto Type
-//Person.prototype
-// Person constructor
-function Person(firstname, lastname, dob) {
+//ProtoType Inheritance
+//Person Prototype constructior
+
+function Person(firstname, lastname) {
   this.firstname = firstname;
   this.lastname = lastname;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  // this.calculateAge = function() {
-  //   const diff = Date.now() - this.birthday.getTime();
-  //   const ageDate = new Date(diff);
-  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
-  // };
 }
 
-const saurav = new Person("saurav", "pal", "01-05-1996");
-const gaurav = new Person("gaurav", "pal", "july 1 1994");
-
-console.log(gaurav);
-
-// calculateAge
-Person.prototype.calculateAge = function() {
-  const diff = Date.now() - this.birthday.getTime();
-  const ageDate = new Date(diff);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-};
-//full name
-Person.prototype.getFullname = function() {
+Person.prototype.greating = function() {
   return this.firstname + " " + this.lastname;
 };
 
-//Gets JobTitle
-Person.prototype.getJobTitle = function(jobTitle) {
-  this.lastname = jobTitle;
+const person1 = new Person("gaurav", "pal");
+console.log(person1.greating());
+
+// Customer cconstructor
+function Customer(firstname, lastname, phnumber, membership) {
+  Person.call(this, firstname, lastname);
+  this.phnumber = phnumber;
+  this.membership = membership;
+}
+
+// Inhinherit ther Person Prototype method
+Customer.prototype = Object.create(Person.prototype);
+// make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+//create Customer
+const customer1 = new Customer("saurav", "pal", "3223232", "asd");
+console.log(customer1);
+
+//Customer Greating
+Customer.prototype.greating = function() {
+  return "hello Customer " + this.firstname + " " + this.lastname;
 };
-
-console.log(saurav.getFullname());
-console.log(saurav.calculateAge());
-
-console.log(gaurav.getFullname());
-console.log(gaurav.calculateAge());
-
-gaurav.getJobTitle("Programmer");
-console.log(gaurav.getFullname());
-// check __proto__
-console.log(gaurav.hasOwnProperty("firstname"));
-console.log(gaurav.hasOwnProperty("getJobTitle"));
+console.log(customer1.greating());
