1d15b92ffe08ec617fee377eaca2ba72f63dc172
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-17 16:34:14 +0530

    add put method

--- Ajax-custom-lab/app.js
+++ Ajax-custom-lab/app.js
@@ -1,11 +1,49 @@
 const http = new easyHTTP();
 
 // get Posts
-http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+// http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+// get single Post
+// http.get("https://jsonplaceholder.typicode.com/posts/1", function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//create data
+const data = {
+  title: "custome Post",
+  body: "This is custome Post"
+};
+
+// create Post
+// http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//update Post
+
+http.post("https://jsonplaceholder.typicode.com/posts/1", data, function(
+  err,
+  post
+) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
   }
 });
-//console.log(posts);
