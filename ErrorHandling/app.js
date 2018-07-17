c23babf3e7ee99f9e7e07e77d67e546077f4d1fd
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-17 21:23:52 +0530

    Regular Expression three

--- ErrorHandling/app.js
+++ ErrorHandling/app.js
@@ -16,7 +16,26 @@ re = /gre?a?y/i; // Optional Cheracter
 
 re = /gre?a?y\?/i; // escape Cheracter
 
-const str = "Gray";
+// Brackets [] - charecter sets
+re = /gr[ae]y/i; // Must Be An a Or e
+re = /[GF]ray/i; // Must Be An G Or F
+re = /[^GF]ray/i; // Match anyThing except a G or F
+re = /[A-Z]ray/i; // Match anyThing any UPPERcase Latter A to Z
+re = /[a-z]ray/i; // Match anyThing any lower case Latter a to z
+re = /[A-Za-z]ray/i; // Match any latter in any case
+re = /[0-9]ray/; // match 0 - 9 any digit
+
+// Braces {} - Quantifires
+re = /Hel{2}o/i; /// must occur exactly {m} amount
+
+re = /Hel{2,4}o/i; // must occur exactly {m} amount of Times
+
+re = /Hel{2,}o/i; // must occur atlist m amount of Times
+// Parenthese() -Grouping
+//sytring to match
+re = /([0-9]x){3}/;
+
+const str = "3x3x3x";
 const result = re.exec(str);
 console.log(result);
 
