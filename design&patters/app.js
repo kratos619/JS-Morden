598b8b0949fb1080e460c5732d70ddf850a7bbdf
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-19 14:40:18 +0530

    design and patterns and reval patterns

--- design&patters/app.js
+++ design&patters/app.js
@@ -8,19 +8,47 @@
 // })();
 
 // standard module pattern
-const UICtrl = (function() {
-  let text = "hello World";
-  const changeText = function() {
-    const elment = document.querySelector("h1");
-    elment.textContent = text;
-  };
+// const UICtrl = (function() {
+//   let text = "hello World";
+//   const changeText = function() {
+//     const elment = document.querySelector("h1");
+//     elment.textContent = text;
+//   };
+
+//   return {
+//     callChaneText: function() {
+//       changeText();
+//       console.log(text);
+//     }
+//   };
+// })();
+
+// UICtrl.callChaneText();
+
+// revaling Module And Pattern
+const ItemCtrl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log("item Added...");
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
 
   return {
-    callChaneText: function() {
-      changeText();
-      console.log(text);
-    }
+    add: add,
+    get: get
   };
 })();
 
-UICtrl.callChaneText();
+ItemCtrl.add({ id: 1, name: "gaurav" });
+//console.log(ItemCtrl.get(1));
+ItemCtrl.add({ id: 2, name: "saurabh" });
+console.log(ItemCtrl.get(2));
+// ItemCtrl.add({ id: 1, name: "gaurav" });
+// console.log(ItemCtrl.get(1));
